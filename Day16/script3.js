89e405abd1d3e0595df3023be7de5bad7da7e593
manikandan28102003-jpg <manikandan28102003@gmail.com>
2025-10-24 20:50:50 +0530

    Day16

---
+++ Day16/script3.js
@@ -0,0 +1,9 @@
+// Declare variables
+let name = "Mani";
+let age = 21;
+let city = "salem";
+
+// Display them in the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
