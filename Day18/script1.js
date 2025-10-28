2a1c3bb2e0f31830e26bda3d34c1324805f6c23b
manikandan28102003-jpg <manikandan28102003@gmail.com>
2025-10-28 21:09:43 +0530

    Day18

---
+++ Day18/script1.js
@@ -0,0 +1,6 @@
+let num=5
+if (num > 0)
+  console.log("The number is positive.");
+ else  (num < 0) 
+  console.log("The number is negative.");
+
---
+++ Day18/script5.js
@@ -0,0 +1,8 @@
+let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Friday":
+    console.log("Weekend is coming!");
+    break;}
